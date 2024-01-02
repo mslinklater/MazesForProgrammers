139e4fd641b2badd93f334e7cde256d8d1df335a
Martin Linklater <mslinklater@gmail.com>
2024-01-02 13:51:12 +0000

    Started Dijkstras

--- ruby/binary_tree_demo.rb
+++ ruby/binary_tree_demo.rb
@@ -1,7 +1,9 @@
 require 'grid'
 require 'binary_tree'
 
-grid = Grid.new(4, 4)
+grid = Grid.new(40, 40)
 BinaryTree.on(grid)
 
 puts grid
+img = grid.to_png
+img.save "maze_binary_tree.png"
--- ruby/sidewinder_demo.rb
+++ ruby/sidewinder_demo.rb
@@ -1,7 +1,9 @@
 require 'grid'
 require 'sidewinder'
 
-grid = Grid.new(8, 8)
+grid = Grid.new(32, 32)
 Sidewinder.on(grid)
 
 puts grid
+img = grid.to_png
+img.save "maze_sidewinder.png"
