f884f106a181ca8521078991ea075f44a9d10e33
Martin Linklater <mslinklater@gmail.com>
2026-06-18 07:47:13 +0100

    End of chapter 2

--- ruby/binary_tree_demo.rb
+++ ruby/binary_tree_demo.rb
@@ -3,4 +3,6 @@ require 'binary_tree'
 
 grid = Grid.new(10, 10)
 BinaryTree.on(grid)
+img = grid.to_png
+img.save('binary_tree.png')
 puts grid
--- ruby/sidewinder_demo.rb
+++ ruby/sidewinder_demo.rb
@@ -3,4 +3,6 @@ require 'sidewinder'
 
 grid = Grid.new(10, 10)
 Sidewinder.on(grid)
+img = grid.to_png
+img.save('sidewinder.png')
 puts grid
